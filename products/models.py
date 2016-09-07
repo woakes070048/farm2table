433a6ea62be0c44de13b2c5f1991fae1e619288d
edziegle <cerna_gora@me.com>
2016-09-06 21:44:18 -0600

    fleshed out products model and created migration, added Pillow to installed packages for ImageField usage

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    """The model for various products that farmers/producers sell"""
+    name = models.CharField(max_length=150)
+    type = models.CharField(max_length=70)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    date_added = models.DateTimeField()
