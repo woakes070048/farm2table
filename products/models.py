abe2fbe6327c10292c61fbfcc6f33413f207154f
edziegle <cerna_gora@me.com>
2016-09-06 22:21:35 -0600

    improved Products model and enabled Admin panel editting of Products

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     """The model for various products that farmers/producers sell"""
+    # TODO
     name = models.CharField(max_length=150)
     type = models.CharField(max_length=70)
     image = models.ImageField(blank=True, null=True)
@@ -12,3 +13,7 @@ class Product(models.Model):
     quantity = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Override the str function to return the Product's name"""
+        return self.name
