7e954810246553580ec6067c517cc44812b609f1
edziegle <cerna_gora@me.com>
2016-09-27 13:48:46 -0600

    removed accounts app as it is superceded by django-registration, configured user registration w/ email and user login and logout

--- farm2table/settings.py
+++ farm2table/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'farm2table', 'static'),
 )
+
+LOGOUT_REDIRECT_URL = '/'
--- farm2table/urls.py
+++ farm2table/urls.py
@@ -13,17 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from accounts.views import user_registration, UserProfileView
 from products.views import list_products
 
 urlpatterns = [
     url(r'^logout/', auth_views.logout, name="logout"),
-    url(r'^login/', auth_views.login, {'template_name': 'login.html'}, name="login"),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^new-user/$', user_registration, name='user_registration'),
-    url(r'^profile/$', UserProfileView.as_view(), name="profile"),
     url(r'^$', list_products, name="products"),
 ]
